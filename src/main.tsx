ccb24dfbf0de1a8013c7daf9016134817a975574
Jicheng Zhi <jzhi0004@student.monash.edu>
2024-03-04 15:05:34 +1100

    fix: comment out Auth0Provide because http is used in production

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
@@ -6,15 +5,15 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    {/*<Auth0Provider*/}
+    {/*  domain={import.meta.env.VITE_AUTH0_DOMAIN}*/}
+    {/*  clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}*/}
+    {/*  authorizationParams={{*/}
+    {/*    redirect_uri: window.location.origin,*/}
+    {/*    audience: import.meta.env.VITE_AUTH0_AUDIENCE,*/}
+    {/*  }}*/}
+    {/*>*/}
+    <App />
+    {/*</Auth0Provider>*/}
   </React.StrictMode>,
 );
